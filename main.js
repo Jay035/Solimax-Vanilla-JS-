cd414a271d12e23202aa08661bf11c539b66bba5
Codexjay <jaydencolins08@gmail.com>
2023-01-18 03:00:18 +0100

    UPDATE

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ const input = document.getElementById("input");
 const menu = document.querySelector(".menu");
 const menuList = document.querySelectorAll(".menu-item");
 
-window.addEventListener("load", function(){
-    setTimeout(() => {
-
-        document.getElementById('preloader').style.display = 'none';
-    }, 2000)
+// HIDE LOADING ANIMATION
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    document.getElementById("preloader").style.display = "none";
+  }, 2000);
 });
 
-input.addEventListener('click', () => {
-    menu.classList.toggle("active");
-})
+// DISPLAY MENU ON MOBILE
+input.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
 
-menuList.forEach(item => {
-    item.addEventListener('click', () => {
-        menu.classList.toggle("active");
-        console.log(input.checked)
-        input.checked = false;
-    })
-})
+menuList.forEach((item) => {
+  item.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    console.log(input.checked);
+    input.checked = false;
+  });
+});
