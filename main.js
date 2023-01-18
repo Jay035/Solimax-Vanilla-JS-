657330fc224650e09263d1842bbb961de993e937
Codexjay <jaydencolins08@gmail.com>
2023-01-18 14:00:47 +0100

    added copy functionality

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const input = document.getElementById("input");
 const menu = document.querySelector(".menu");
 const menuList = document.querySelectorAll(".menu-item");
+const successMessage = document.querySelector(".success-message");
+const errorMessage = document.querySelector(".error-message");
 
 // HIDE LOADING ANIMATION
 window.addEventListener("load", function () {
@@ -21,3 +23,23 @@ menuList.forEach((item) => {
     input.checked = false;
   });
 });
+
+function copyToClipboard() {
+  let text = "0xb1957BDbA889686EbdE631DF970ecE6A7571A1B6";
+  navigator.clipboard.writeText(text).then(
+    function () {
+      console.log("Async: copying address was successful");
+      successMessage.classList.toggle("hidden");
+      setTimeout(() => {
+        successMessage.classList.toggle("hidden");
+      }, 4000);
+    },
+    function (err) {
+      console.error("Aync could not copy text:", err);
+      errorMessage.classList.toggle("hidden");
+      setTimeout(() => {
+        errorMessage.classList.toggle("hidden");
+      }, 4000);
+    }
+  );
+}
